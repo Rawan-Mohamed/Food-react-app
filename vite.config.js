884fb617a4b473f2f0e2f4a5a394796cf89687b7
rawan.mohamed7129@gmail.com <rawan.mohamed7129@gmail.com>
2023-12-10 03:32:52 +0200

    update

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base:/Food-react-app/,
   plugins: [react()],
-  // base:/Food-react-app/
+  
 })
